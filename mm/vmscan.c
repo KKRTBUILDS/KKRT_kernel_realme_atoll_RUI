63f50ed135a61bba50103796dd9242c5933bd12c
pri0818 <priyanshusinghal0818@gmail.com>
2022-09-11 19:20:29 +0000

    treewide: Oplus: Remove OPLUS_FEATURE_MULTI_KSWAPD
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: priiii0818 <priyanshusinghal0818@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -68,10 +68,6 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/vmscan.h>
-
-#if defined(OPLUS_FEATURE_MULTI_KSWAPD) && defined(CONFIG_OPLUS_MULTI_KSWAPD)
-#include <linux/multi_kswapd.h>
-#endif /*OPLUS_FEATURE_MULTI_KSWAPD*/
 struct scan_control {
 	/* How many pages shrink_list() should reclaim */
 	unsigned long nr_to_reclaim;
@@ -3694,11 +3690,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
  * If there are applications that are active memory-allocators
  * (most normal use), this basically shouldn't matter.
  */
-#if defined(OPLUS_FEATURE_MULTI_KSWAPD) && defined(CONFIG_OPLUS_MULTI_KSWAPD)
- int kswapd(void *p)
-#else
 static int kswapd(void *p)
-#endif /*OPLUS_FEATURE_MULTI_KSWAPD*/
 {
 	unsigned int alloc_order, reclaim_order;
 	unsigned int classzone_idx = MAX_NR_ZONES - 1;
@@ -3863,9 +3855,6 @@ unsigned long shrink_all_memory(unsigned long nr_to_reclaim)
    restore their cpu bindings. */
 static int kswapd_cpu_online(unsigned int cpu)
 {
-#if defined(OPLUS_FEATURE_MULTI_KSWAPD) && defined(CONFIG_OPLUS_MULTI_KSWAPD)
-	return kswapd_cpu_online_ext(cpu);
-#else
 	int nid;
 
 	for_each_node_state(nid, N_MEMORY) {
@@ -3879,7 +3868,6 @@ static int kswapd_cpu_online(unsigned int cpu)
 			set_cpus_allowed_ptr(pgdat->kswapd, mask);
 	}
 	return 0;
-#endif /*OPLUS_FEATURE_MULTI_KSWAPD*/
 }
 
 /*
@@ -3888,9 +3876,6 @@ static int kswapd_cpu_online(unsigned int cpu)
  */
 int kswapd_run(int nid)
 {
-#if defined(OPLUS_FEATURE_MULTI_KSWAPD) && defined(CONFIG_OPLUS_MULTI_KSWAPD)
-		return kswapd_run_ext(nid);
-#else
 	pg_data_t *pgdat = NODE_DATA(nid);
 	int ret = 0;
 
@@ -3906,7 +3891,6 @@ int kswapd_run(int nid)
 		pgdat->kswapd = NULL;
 	}
 	return ret;
-#endif /*OPLUS_FEATURE_MULTI_KSWAPD*/
 }
 
 /*
@@ -3915,16 +3899,12 @@ int kswapd_run(int nid)
  */
 void kswapd_stop(int nid)
 {
-#if defined(OPLUS_FEATURE_MULTI_KSWAPD) && defined(CONFIG_OPLUS_MULTI_KSWAPD)
-	return kswapd_stop_ext(nid);
-#else
 	struct task_struct *kswapd = NODE_DATA(nid)->kswapd;
 
 	if (kswapd) {
 		kthread_stop(kswapd);
 		NODE_DATA(nid)->kswapd = NULL;
 	}
-#endif /*OPLUS_FEATURE_MULTI_KSWAPD*/
 }
 
 static int __init kswapd_init(void)
